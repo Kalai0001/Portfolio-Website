aedf6c54ff3dfc2d15553781a7c769943269229f
Kalai0001 <kalai1172000@gmail.com>
2026-06-12 15:23:23 +0530

    Updated portfolio website

---
+++ script.js
@@ -0,0 +1,44 @@
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", () => {
+//     navMenu.classList.toggle("active");
+// });
+
+
+
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// });
+
+// document.querySelectorAll(".nav-menu a").forEach(link => {
+//     link.addEventListener("click", () => {
+//         hamburger.classList.remove("active");
+//         navMenu.classList.remove("active");
+//     });
+// });
+
+
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", () => {
+//     navMenu.classList.toggle("active");
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
